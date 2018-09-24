4ea822063b707c140f9bc33a273b4ed7a98a2307
Lael Santos Oliveira <lael.oliveira@smn.domain>
2018-09-24 16:05:14 -0300

    Testes api tempo wheater

--- dev/server.js
+++ dev/server.js
@@ -3,5 +3,16 @@ var http = require('http');
 http.createServer( function (req, res){
     res.writeHead(200, {'Content-Type':'text-plain'});
     res.end('Hello World');
-}).listen(8080)
+}).listen(3000)
 console.log("Test Hello World comand")
+
+
+var request = require('request');
+
+request('https://samples.openweathermap.org/data/2.5/weather?q=Franca,br&appid=b6907d289e10d714a6e88b30761fae22', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  var parsedWeather = JSON.parse(body);
+  console.log('A temperatura atual Franca é ' + parsedWeather['main']['temp']); // Print the Temperature in the city of São Paulo
+ 
+});
